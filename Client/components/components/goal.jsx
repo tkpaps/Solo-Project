ec2a332eee11244d14a44c7dba117f046dde039b
Thomas Pappas <thomaskpappas@gmail.com>
2023-06-27 23:00:20 -0400

    final Tue changes

--- Client/components/components/goal.jsx
+++ Client/components/components/goal.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const GoalComponent = ({ goal }) => {
+const GoalComponent = ({ goal, times }) => {
 
   const [count, setCount] = useState(0);
       
@@ -11,6 +11,7 @@ const GoalComponent = ({ goal }) => {
   return (
     <div>
       <p>{goal}</p>
+      <p>Number of Times: {times}</p>
       <p>Count: {count}</p>
       <button onClick={handleIncrement}>+</button>
     </div>
--- Client/components/pages/homepage.jsx
+++ Client/components/pages/homepage.jsx
@@ -5,17 +5,23 @@ function HomePage() {
 
   const [showGoal, setShowGoal] = useState(false);
   const [inputValue, setInputValue] = useState('');
+  const [goalAmount, setGoalAmount] = useState(0);
   const [goalList, setGoalList] = useState([]);
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
 
+  const handleGoalAmountChange = (e) => {
+    setGoalAmount(Number(e.target.value));
+  };
+
   const handleButtonClick = (e) => {
     e.preventDefault();
     if (inputValue.trim() !== '') {
-      setGoalList((prevGoals) => [...prevGoals, inputValue]);
+      setGoalList((prevGoals) => [...prevGoals, {goal: inputValue, times: goalAmount}]);
       setInputValue('');
+      setGoalAmount(0);
       setShowGoal(true);
     }
   };
@@ -25,13 +31,15 @@ function HomePage() {
     <div>
       <h1>Set Your Goals Here!</h1>
       <form>
-        <input value={inputValue} onChange={handleInputChange} ></input>
-        <input ></input>
+        <input value={inputValue} onChange={handleInputChange} placeholder="your goal here"></input>
+        <p>Enter the number of times you want to accomplish your goal</p>
+        <input value={goalAmount} onChange={handleGoalAmountChange} type="number" placeholder="Enter the number of times you want to accomplish this goal"></input>
+        <p></p>
         <button onClick={handleButtonClick}>Add Goal</button>
       </form>
       {showGoal && 
-      goalList.map((goal, index) => (
-        <GoalComponent key={index} goal={goal}/>
+      goalList.map((goal, amount, index) => (
+        <GoalComponent key={index} goal={goal.goal} times={goal.times}/>
       ))}
     </div>
   );
